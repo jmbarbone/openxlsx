caaba73af79c828a5a4be6bde04e19fd824d20d1
jmbarbone <jmbarbone@gmail.com>
2022-01-15 10:59:05 -0500

    adds formula helpers

--- R/formula.R
+++ R/formula.R
@@ -1,4 +1,29 @@
 
+#' Workbook formula
+#' 
+#' Wrapper function for the workbookFormula class
+#' 
+#' @param x A vector of values
+#' @param array Is this an array formula? (not yet implemented)
+#' @returns A vector with class `workbookFormula`
+#' @export
+workbook_formula <- function(x, array = FALSE) {
+  # do we need class(x) or just c("workbookFormula", "character")?
+  structure(x, class = c(class(x), "workbookFormula"), array = array)
+}
+
+# make sure this isn't converted for anything else
+
+#' @export
+as.character.workbookFormula <- function(x, ...) {
+  as.character.default(x)
+}
+
+#' @export
+as.character.workbookArrayFormula <- function(x, ...) {
+  as.character.default(x)
+}
+
 # create warnings about deprecation
 warn_formula <- function() {
   # TODO remove after deprecation
